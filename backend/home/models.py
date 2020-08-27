4c0c1288b601f89a4b0341493902bd4a5b90bc37
Crowdbotics <team@crowdbotics.com>
2020-08-27 09:31:40 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,27 +7,35 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    test = models.OneToOneField(
+        "home.CustomText",
+        on_delete=models.SET_DEFAULT,
+        default="FFFF",
+        null=True,
+        blank=True,
+        related_name="homepage_test",
+    )
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
--- backend/temp_27_aug_dev_9484/wsgi.py
+++ backend/temp_27_aug_dev_9484/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temp_27_aug_dev_9484.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "temp_27_aug_dev_9484.settings")
 
 application = get_wsgi_application()
